f8820f48f607bf829d6d83712eeef959eaae3acd
ismael <ismaelsilva@kofre.com.br>
2019-04-03 01:41:56 -0300

    PI4_Farol 0.0.5
    !Feito{
    - Adicionei algumas Strings que estavam faltando
    - retrabalhei o menu lateral para ficar mais fiel ao protótipo
    -enxuguei a sujeira criada pelos códigos gerados na tela de login.
    -Adicionei a função para usar o botão de login como protótipo.
    }
    !A fazer{
    -terminar Strings.xml
    -colocar a parte de logout da barra lateral no rodapé
    -enxugar a sujeira de código da tela mainMenu.
    }

--- app/src/main/java/com/example/pi4/mainMenu.java
+++ app/src/main/java/com/example/pi4/mainMenu.java
@@ -19,7 +19,7 @@ public class mainMenu extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_main_menu2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
