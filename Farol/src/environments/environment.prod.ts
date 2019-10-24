a6be9eed57470e40f1d689dc687c8a8130c9d665
Ismael Carvalho <ismael_profissional@hotmail.com>
2019-10-24 18:47:32 -0300

    alterações necessárias para usar o gh-pages

--- Farol/src/environments/environment.prod.ts
+++ Farol/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBcu0QmFo0eIy5HCD0sMr5UXqOfX5LvQn8",
+    authDomain: "com.slumdev.farol",
+    databaseURL: "https://farol-cecee.firebaseio.com",
+    projectId: "farol-cecee",
+    storageBucket: "farol-cecee.appspot.com",
+    messagingSenderId: "827662796759"
+  }
 };
