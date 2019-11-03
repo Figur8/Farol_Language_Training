e7b55ee6df8699a1b9a369d83720e63d3c9c8d6d
Ismael Carvalho <ismael_profissional@hotmail.com>
2019-11-03 20:07:09 -0300

    Chart JS adicionado

--- Farol/src/app/feedback/feedback.page.ts
+++ Farol/src/app/feedback/feedback.page.ts
@@ -1,33 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js'; 
 
 @Component({
   selector: 'app-list',
   templateUrl: 'feedback.page.html',
   styleUrls: ['feedback.page.scss']
 })
+
 export class FeedBackPage implements OnInit {
-  private selectedItem: any;
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
-  ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
-  constructor() {
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+  @ViewChild('barChart', { static: true }) barChart;  
+  bars: any;
+  colorArray: any;
+
+  ionViewDidEnter() {
+    this.createBarChart();
+  }
+
+  
+  createBarChart() {
+    this.bars = new Chart(this.barChart.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'],
+        datasets: [{
+          label: 'Viewers in millions',
+          data: [2.5, 3.8, 5, 6.9, 6.9, 7.5, 10, 17],
+          backgroundColor: 'rgb(38, 194, 129)', // array should have same number of elements as number of dataset
+          borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
   }
 
   ngOnInit() {
